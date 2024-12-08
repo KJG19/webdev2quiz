b3297509544af2042a9a8a9980fd6fead49fbd2b
KJG19 <kyleguenter19@gmail.com>
2024-12-08 16:12:13 -0700

    WebDev2 Quiz
    Created a website that has two pages, a home page saying start quiz and a quiz page that is a 10 question quiz that pulls the questions from a json file.

---
+++ app/pages/page.js
@@ -0,0 +1,9 @@
+import Quiz from "./quiz";
+
+export default function MainPage() {
+  return (
+    <main>
+      <Quiz />
+    </main>
+  );
+}
---
+++ app/pages/quiz.js
@@ -0,0 +1,80 @@
+"use client";
+
+import { useState } from "react";
+import questions from "./questions";
+
+export default function QuizPage() {
+  const [answers, setAnswers] = useState({});
+
+  const handleOptionChange = (questionId, selectedOption) => {
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [questionId]: selectedOption,
+    }));
+  };
+
+  const handleSubmit = () => {
+    let grade = 0;
+
+    questions.forEach((question) => {
+      if (answers[question.id] === question.answer) {
+        grade += 1;
+      }
+    });
+
+    alert(`You got ${grade}/${questions.length} !!`);
+
+    setAnswers({});
+  };
+
+  return (
+    <main className="min-h-screen bg-gray-100 p-5">
+      <h1 className="text-center text-4xl font-bold text-green-700 p-5">
+        Kyle's Coding Quiz
+      </h1>
+      <form className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg p-8 space-y-8">
+        {questions.map((question) => (
+          <div key={question.id} className="space-y-4">
+            <h3 className="text-xl font-semibold text-gray-800">
+              {question.question}
+            </h3>
+            {question.image && (
+              <img
+                src={question.image}
+                alt={`${question.id}`}
+                className="w-full max-wd-md mx-auto rounded-lg shadow-md"
+              />
+            )}
+            <div className="space-y-2">
+              {question.options.map((option, index) => (
+                <label
+                  key={index}
+                  className="flex items-center space-x-3 cursor-pointer"
+                >
+                  <input
+                    type="radio"
+                    name={`question-${question.id}`}
+                    value={option}
+                    onChange={() => handleOptionChange(question.id, option)}
+                    checked={answers[question.id] === option}
+                    className="m-1 h-4 w-4 "
+                  />
+                  <span className="text-gray-700">{option}</span>
+                </label>
+              ))}
+            </div>
+          </div>
+        ))}
+        <div className="flex justify-center">
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+}
